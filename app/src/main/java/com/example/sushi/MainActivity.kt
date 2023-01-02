036ebe28b77f0fb7c7632bfe656211d4fd48d7af
theeranat.sre <theeranat.sre@turbo.co.th>
2023-01-02 20:41:04 +0700

    new project

--- app/src/main/java/com/example/sushi/MainActivity.kt
+++ app/src/main/java/com/example/sushi/MainActivity.kt
@@ -3,17 +3,21 @@ package com.example.sushi.main
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
 import com.example.sushi.login.LoginActivity
 import java.lang.NullPointerException
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         try {
             this.supportActionBar!!.hide()
         } catch (e: NullPointerException) {
         }
-        val intentToLogin = Intent(this, LoginActivity::class.java)
-        startActivity(intentToLogin)
+//        val intentToLogin = Intent(this, LoginActivity::class.java)
+//        startActivity(intentToLogin)
     }
 }
--- app/src/main/java/com/example/sushi/poker/PokerViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.sushi.poker
-
-import androidx.lifecycle.ViewModel
-
-class PokerViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
